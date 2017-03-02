89e03a5ee19c300f4b5bbc0a5d311baa7be9323e
Dirk Eddelbuettel <edd@debian.org>
2017-03-01 19:47:17 -0600

    initial commit of doxygen documentation

---
+++ docs/search/typedefs_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['identitylayer',['IdentityLayer',['../namespacemlpack_1_1ann.html#a6d2f8fd261b4ea582c21ffa66dce032e',1,'mlpack::ann']]],
+  ['infobinarytreetype',['InfoBinaryTreeType',['../classmlpack_1_1tree_1_1HoeffdingTreeModel.html#ac37ace0bacc02d33311f3adae2645437',1,'mlpack::tree::HoeffdingTreeModel']]],
+  ['infohoeffdingtreetype',['InfoHoeffdingTreeType',['../classmlpack_1_1tree_1_1HoeffdingTreeModel.html#aa5fb55931696090698cdfdabcbecbd7c',1,'mlpack::tree::HoeffdingTreeModel']]]
+];
